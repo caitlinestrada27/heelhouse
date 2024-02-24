82e63117d34602adf967358baf463dcb0b478c1c
Sanjana Gopalswamy <sgopal@unc.edu>
2024-02-24 15:24:01 -0500

    swipe animation works

--- Heel House/Heel House/ContentView.swift
+++ Heel House/Heel House/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Heel House
 //
-//  Created by Caitlin Estrada on 2/24/24.
+//  Created by Caitlin Estrada, Sadie Sawyer, Carolina Bryan, Sanjana Gopalswamy on 2/24/24.
 //
 
 import SwiftUI
@@ -68,77 +68,61 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
-}
-
 struct CardView: View {
     @State var card: Card
     let cardGradient = Gradient(colors: [Color.black.opacity(0), Color.black.opacity(0.5)])
+    let screenWidth = UIScreen.main.bounds.width
+    
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            Image(card.imageName)
-                .resizable()
-            LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
-            VStack {
-                Spacer()
-                VStack(alignment: .leading){
-                    HStack {
-                        Text(card.name).font(.largeTitle).fontWeight(.bold)
-                        Text(String(card.age)).font(.title)
+        GeometryReader { geometry in
+            ZStack(alignment: .topLeading) {
+                Image(card.imageName)
+                    .resizable()
+                LinearGradient(gradient: cardGradient, startPoint: .top, endPoint: .bottom)
+                VStack {
+                    Spacer()
+                    VStack(alignment: .leading){
+                        HStack {
+                            Text(card.name).font(.largeTitle).fontWeight(.bold)
+                            Text(String(card.age)).font(.title)
+                        }
+                        Text(card.bio)
                     }
-                    Text(card.bio)
                 }
+                .padding()
+                .foregroundColor(.white)
             }
-            .padding()
-            .foregroundColor(.white)
-        }
-        // match or nope
-        HStack {
-            Image("match")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150)
-                .opacity(Double(card.x/10 - 1))
-            Spacer()
-            Image("pass")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150)
-                .opacity(Double(card.x/10 * -1 - 1))
-        }
-        .cornerRadius(8)
-        // Step 1 - ZStack follows the coordinates of the card model
-        .offset(x: card.x, y: card.y)
-        .rotationEffect(.init(degrees: card.degree))
-        // Step 2 - gesture recognizer updaets the coordinate values of the card model
-        .gesture ( // Needs debugging
-            
-            DragGesture()
-            
-                .onChanged { value in
-                    // user is dragging the view
-                    withAnimation(.default) {
-                        card.x = value.translation.width
-                        card.y = value.translation.height
-                        card.degree = 7 * (value.translation.width > 0 ? 1 : -1)
-                    }
-                }
-            
-                .onEnded { value in
-                    // do something when the user stops dragging
-                    withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 8, initialVelocity: 0)) { // creates bouncing effect
-                        switch value.translation.width {
-                            case 0...100: // if value is 0-100, card returns to original position
-                                card.x = 0; card.degree = 0; card.y = 0;
-                            case let x where x > 100: // if user swipes right, card leaves screen
-                                card.x = 500; card.degree = 12
-                            case (-100)...(-1): // if user swipes left, card leaves screen
-                                card.x = -500; card.y = 0
-                            default: card.x = 0; card.y = 0
+            .cornerRadius(8)
+            .offset(x: card.x, y: card.y)
+            .rotationEffect(.init(degrees: card.degree))
+            .gesture (
+                DragGesture()
+                    .onChanged { value in
+                        withAnimation(.default) {
+                            card.x = value.translation.width
+                            card.y = value.translation.height
+                            card.degree = 7 * (value.translation.width > 0 ? 1 : -1)
                         }
                     }
-                }
-        )
+                    .onEnded { value in
+                        withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 8, initialVelocity: 0)) {
+                            // let screenEdgeX = geometry.size.width * 0.8
+                            switch value.translation.width {
+                                case 0...100:
+                                    card.x = 0; card.degree = 0; card.y = 0
+                                case let x where x > 100:
+                                    card.x = 500; card.degree = 12 // Moved off to the right
+                                case (-100)...(-1):
+                                    card.x = -500; card.degree = -12 // Moved off to the left
+                                case let x where x < -100:
+                                card.x = -500; card.degree = -12
+                                default:
+                                    card.x = 0; card.y = 0
+                            }
+                        }
+                    }
+            )
+            .opacity(card.x == 0 ? 1 : 0) // Hide the card if it's not at the center
+        }
     }
 }
