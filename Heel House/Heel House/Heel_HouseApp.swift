eaffae0d94f31e376c9924042e75bfeb79885cf7
Caitlin Estrada <100039736+caitlinestrada23@users.noreply.github.com>
2024-02-24 11:45:43 -0500

    Create XCode Files

---
+++ Heel House/Heel House/Heel_HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heel_HouseApp.swift
+//  Heel House
+//
+//  Created by Caitlin Estrada on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Heel_HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
