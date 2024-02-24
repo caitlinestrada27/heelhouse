46c75ffcaab9689809d09ce9c80f34f69b35293b
Caitlin Estrada <100039736+caitlinestrada23@users.noreply.github.com>
2024-02-24 18:22:20 -0500

    Profile View

--- Heel House/Heel House/ProfileView.swift
+++ Heel House/Heel House/ProfileView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var isPresentingScreen = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack {
+                Button(action: {}) {
+                    Image("HeelHouseLogo")
+                        .resizable().aspectRatio(contentMode: .fit).frame(height: 45)
+                    
+                }
+            }.padding(.horizontal)
+            
+            Text("Profile")
+                .font(.largeTitle)
+                .foregroundColor(.black)
+                .padding()
+                .background(Color.white)
+                .edgesIgnoringSafeArea(.all)
+            Button(action: {
+                isPresentingScreen = true
+            }) {
+                Image(systemName: "house.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .padding()
+            }
+            
+            //SHOWCASE PROFILE HERE
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .fullScreenCover(isPresented: $isPresentingScreen) {
+            ContentView()
+        }
     }
 }
 
