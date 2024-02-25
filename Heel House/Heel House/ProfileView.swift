4eb01f1bedec1688a1d367985d69c34fca05fe5d
Sanjana Gopalswamy <sgopal@unc.edu>
2024-02-24 23:38:28 -0500

    fixed profile view user data input

--- Heel House/Heel House/ProfileView.swift
+++ Heel House/Heel House/ProfileView.swift
@@ -8,24 +8,28 @@
 import Foundation
 import SwiftUI
 
+class UserData: ObservableObject {
+    // User attributes
+    @Published var firstName: String = ""
+    @Published var lastName: String = ""
+    @Published var age: String = ""
+    @Published var housing: String = ""
+    @Published var major: String = ""
+    @Published var sleepSchedule: Int = 0
+    @Published var bio: String = ""
+}
+
 struct ProfileView: View {
+    @StateObject var userData = UserData()
     @State private var presentMatchView = false
     @State private var presentProfileView = false
     @State private var presentContentView = false
-    
-    // User attributes
-    @State var firstName: String = ""
-    @State var lastName: String = ""
-    @State var age: String = ""
-    @State var housing: String = ""
-    @State var major: String = ""
-    @State var sleepSchedule: Int = 0
-    @State var bio: String = ""
     
     var body: some View {
         //user can view matches they swiped right on here
         // create list from matches where card.x > 500
         VStack{
+            Spacer()
             HStack {
                 Button(action: {}) {
                     Image("HeelHouseLogo")
@@ -36,42 +40,45 @@ struct ProfileView: View {
             Text("Profile")
                 .font(.largeTitle)
                 .foregroundColor(.black)
-                .padding()
                 .background(Color.white)
                 .edgesIgnoringSafeArea(.all)
             
-            // User Info
-            VStack(alignment: .leading) {
-                Text("First Name: \(firstName)")
-                TextField("First Name", text: $firstName)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Last Name: \(lastName)")
-                TextField("Last Name", text: $lastName)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Age: \(age)")
-                TextField("Age",text: $age)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Housing: \(housing)")
-                TextField("Housing",text: $housing)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Major: \(major)")
-                TextField("Major", text: $major)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Bio: \(bio)")
-                TextField("Bio",text: $bio)
-                    .disableAutocorrection(true)
-                    .padding()
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            HStack{
+                Spacer()
+                // User Info
+                VStack(alignment: .leading) {
+                    Text("First Name: \(userData.firstName)")
+                    TextField("First Name", text: $userData.firstName)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("Last Name: \(userData.lastName)")
+                    TextField("Last Name", text: $userData.lastName)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("Age: \(userData.age)")
+                    TextField("Age",text: $userData.age)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("Housing: \(userData.housing)")
+                    TextField("Housing",text: $userData.housing)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("Major: \(userData.major)")
+                    TextField("Major", text: $userData.major)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("Bio: \(userData.bio)")
+                    TextField("Bio",text: $userData.bio)
+                        .disableAutocorrection(true)
+                        .padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                Spacer()
             }
             
             Spacer()
